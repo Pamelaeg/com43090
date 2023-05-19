05a1da4b744a9ab8490381a5b8832f2635ee4548
Pamela Garcia <pamelaeg1992@gmail.com>
2023-05-19 19:23:45 -0300

    correccion

--- js/main.js
+++ js/main.js
@@ -11,24 +11,55 @@
 //    b) Salir 
 // 7 - Saludo de despedida
 
-function calcularPorcent(porcentaje,valor,total) {
-    return porcentaje*valor/100
+let respuesta = "rojo"
+let prueba = false
+
+for (let i = 0; i <= 2; i++) {
+    let verificacion = prompt(
+        "Hola! Soy una demo de una calculadora interactiva, vamos a corroborar que eres humano: El color del tomate es..."
+        )
+    if (verificacion === respuesta) {
+        alert(
+            "Corroboramos que eres humano. COMENZEMOS!"
+            );
+        prueba = true;
+
+        break;
+    } else {
+        alert("Lo siento, ese no es el color de los tomates. Vuelve a intentarlo")
+    }
 }
 
-let saludo = alert("Hola! Soy una demo de una calculadora interactiva");
-
-let calculo=prompt("Escriba % si desea calcular un porcentaje")
-
-switch (calculo) {
-    case "%":
-        let porcentaje=parseInt(prompt("Complete el porcentaje que desea calcular"))
-        let valor = parseInt(prompt("Complete el número sobre el que sea calcular el " + porcentaje + "%."))
-        let resultado=calcularPorcent (porcentaje,valor)
-        alert("El " + porcentaje + " % de " + valor + " es " + resultado)
-        break;
-
-    default: alert("La opción no es válida. Presione F5 para volver a comenzar")
-        break;
+function calcularPorcent(porcentaje, valor, total) {
+    return porcentaje * valor / 100
 }
 
-alert("Gracias! Visitanos en unas semanas para ver los avances")
+function accionSumar(number1, number2) {
+    return number1 + number2
+}
+
+if (prueba) {
+    let calculo = prompt("Escriba % si desea calcular un porcentaje o un signo + si desea sumar dos números");
+    while (calculo != 0) {
+        switch (calculo) {
+            case "%":
+                let porcentaje = parseInt(prompt("Complete el porcentaje que desea calcular"))
+                let valor = parseInt(prompt("Complete el número sobre el que sea calcular el " + porcentaje + "%."))
+                let resultado = calcularPorcent(porcentaje, valor)
+                alert("El " + porcentaje + " % de " + valor + " es " + resultado)
+                break;
+            case "+":
+                let number1 = parseInt(prompt("Ingrese el primer numero que desea sumar"))
+                let number2 = parseInt(prompt("Ahora ingrese el numero que quiere sumarle a " + number1))
+                let resultadoSuma = accionSumar(number1, number2)
+                alert("La suma de " + number1 + " y " + number2 + " es " + resultadoSuma);
+                break;
+            default:
+                alert("Solo puedes seleccionar entre las opciones % y +")
+                break;
+        }
+        calculo = prompt("Escriba % si desea calcular un porcentaje o un signo + si desea sumar dos números o presiona 0 para salir ")
+    }
+} else {
+    alert("Lo siento, corroboramos que no eres un humano y no alcanzas los conocimientos basicos de la inteligencia artificial como para continuar. Gracias de todos modos por tu interes en realizar calculos matemáticos")
+}
